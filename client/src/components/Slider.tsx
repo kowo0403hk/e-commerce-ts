9db508f1e6423e344414d658a6a7276a5057b3ef
Alex Cheung <alexc.sfu@gmail.com>
2022-08-02 04:10:47 -0700

    added slideIndex state to manage the slider movement

--- client/src/components/Slider.tsx
+++ client/src/components/Slider.tsx
@@ -4,10 +4,9 @@ import styled from "styled-components";
 import { sliderItems } from "../data";
 
 const Container = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral
   position: relative;
   overflow: hidden;
 `;
@@ -18,27 +17,33 @@ interface ArrowProps {
 }
 
 const Arrow = styled.div<ArrowProps>`
-width: 50px;
-height: 50px;
-background-color: #fff7f7
-border-radius: 50%
-display: flex;
-justify-content: center;
-align-items: center;
-position: absolute;
-top: 0;
-left: ${(props) => props.direction === "left" && "10px"};
-right: ${(props) => props.direction === "right" && "10px"};
-bottom: 0;
-margin: auto;
-cursor: pointer;
-opacity: 0.5;
-z-index: 2;`;
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
 
-const Wrapper = styled.div`
+interface WrapperProps {
+  slideIndex: number;
+}
+
+const Wrapper = styled.div<WrapperProps>`
   height: 100vh;
   display: flex;
-  transform: translateX(0vw);
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 interface SlideProps {
@@ -76,6 +81,7 @@ const Title = styled.h1`
 
 const Description = styled.p`
   margin: 50px 0px;
+  font-size: 20px;
   font-weight: 500;
   letter-spacing: 3px;
 `;
@@ -90,31 +96,33 @@ const Button = styled.button`
 const Slider: FC = () => {
   const [slideIndex, setSlideIndex] = useState(0);
 
-  const handleClick = (direction: string): void => {};
+  const handleClick = (direction: string): void => {
+    direction === "left"
+      ? setSlideIndex(slideIndex > 0 ? slideIndex - 1 : sliderItems.length - 1)
+      : setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+  };
+
+  const mappedSilderItems = sliderItems.map((item) => {
+    return (
+      <Slide bg={item.bg} key={item.id}>
+        <ImgContainer>
+          <Image src={item.img} />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>{item.title}</Title>
+          <Description>{item.desc}</Description>
+          <Button>SHOW NOW</Button>
+        </InfoContainer>
+      </Slide>
+    );
+  });
+
   return (
     <Container>
       <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftOutlined fontSize="large" />
       </Arrow>
-      <Wrapper>
-        {sliderItems.map((item) => {
-          return (
-            <Slide bg={item.bg}>
-              <ImgContainer>
-                <Image src={item.Image} />
-              </ImgContainer>
-              <InfoContainer>
-                <Title>Summer Sale</Title>
-                <Description>
-                  Don't Compromise on Style! Get a flat 30% off for new
-                  arrivals!
-                </Description>
-                <Button>Shop Now</Button>
-              </InfoContainer>
-            </Slide>
-          );
-        })}
-      </Wrapper>
+      <Wrapper slideIndex={slideIndex}>{mappedSilderItems}</Wrapper>
       <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightOutlined fontSize="large" />
       </Arrow>
