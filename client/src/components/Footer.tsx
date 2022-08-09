8cf63685e2a5ff9caae0eb9b80ec04e9a468bbfa
Alex Cheung <alexc.sfu@gmail.com>
2022-08-08 23:57:33 -0700

    added material ui icons for social media links

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,13 +1,48 @@
+import { Facebook, Instagram, Pinterest, Twitter } from "@material-ui/icons";
 import { FC } from "react";
 import styled from "styled-components";
 
-const Container = styled.div``;
-const Left = styled.div``;
-const Center = styled.div``;
-const Right = styled.div``;
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Footer: FC = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Left>
+        <Logo>Fahsion</Logo>
+        <Description>
+          This is a e-commerce project built with React and TypeScript. For more
+          information, please visit their official websites.
+        </Description>
+        <SocialContainer>
+          <SocialIcon>
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon>
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon>
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon>
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center></Center>
+      <Right></Right>
+    </Container>
+  );
 };
 
 export default Footer;
