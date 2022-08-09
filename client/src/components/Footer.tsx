6b514d1806b4bd41a9c30a401997d2c984b7e96e
Alex Cheung <alexc.sfu@gmail.com>
2022-08-08 23:53:19 -0700

    added initial Footer component layout

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Left = styled.div``;
+const Center = styled.div``;
+const Right = styled.div``;
+
+const Footer: FC = () => {
+  return <Container></Container>;
+};
+
+export default Footer;
