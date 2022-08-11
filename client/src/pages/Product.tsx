3977909a514858ab90a9e4b2797b3d223878bd75
Alex Cheung <alexc.sfu@gmail.com>
2022-08-11 00:29:02 -0700

    added more container elements in product page

--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -1,10 +1,34 @@
 import { FC } from "react";
 import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import NewsLetter from "../components/NewsLetter";
 
 const Container = styled.div``;
 
 const Product: FC = () => {
-  return <Container>Product</Container>;
+  return (
+    <Container>
+      <Announcement />
+      <Navbar />
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Description>
+            Designed by local artisan in Tokyo, Japan. Each thread on the denim
+            tells a story.
+          </Description>
+          <Price>$39.99</Price>
+        </InfoContainer>
+      </Wrapper>
+      <NewsLetter />
+      <Footer />
+    </Container>
+  );
 };
 
 export default Product;
