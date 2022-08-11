627cfdb777279ad24662dde262f151c62beb3dbb
Alex Cheung <alexc.sfu@gmail.com>
2022-08-11 00:40:49 -0700

    added filters to product page

--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -7,6 +7,51 @@ import NewsLetter from "../components/NewsLetter";
 
 const Container = styled.div``;
 
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+  font-weight: 600;
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div``;
+
+const Filter = styled.div``;
+
+const FilterTitle = styled.span``;
+
+const FilterColor = styled.div``;
+
+const FilterSize = styled.select``;
+
+const FilterSizeOption = styled.option``;
+
 const Product: FC = () => {
   return (
     <Container>
@@ -14,15 +59,33 @@ const Product: FC = () => {
       <Navbar />
       <Wrapper>
         <ImgContainer>
-          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+          <Image src="https://github.com/kowo0403hk/e-commerce-ts/blob/main/client/docs/jean.jpeg?raw=true" />
         </ImgContainer>
         <InfoContainer>
           <Title>Denim Jumpsuit</Title>
           <Description>
-            Designed by local artisan in Tokyo, Japan. Each thread on the denim
-            tells a story.
+            Designed by local artisan in Tokyo, Japan. Each line of thread on
+            the denim tells a story.
           </Description>
           <Price>$39.99</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
         </InfoContainer>
       </Wrapper>
       <NewsLetter />
