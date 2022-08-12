26b12977b188cb7d85d88fc42eed88e7990246a4
Alex Cheung <alexc.sfu@gmail.com>
2022-08-12 15:11:48 -0700

    added initial layout of login form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,12 +1,69 @@
-import {FC} from 'react';
+import { FC } from "react";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url(https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=1080&w=1920)
+      center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0 0;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: rgb(10, 186, 181);
+  color: white;
+  cursor: pointer;
+`;
+
+const Link = styled.div``;
 
 const Login: FC = () => {
   return (
-    <Container>Login</Container>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Title>Sign In</Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Button>Login</Button>
+          <Link>Forgot password?</Link>
+          <Link>Create a new account</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Login
+export default Login;
