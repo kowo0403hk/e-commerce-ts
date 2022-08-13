38d64042f9211c57a86382842b0ae7fce5bd5f84
Alex Cheung <alexc.sfu@gmail.com>
2022-08-12 20:45:47 -0700

    refined cart component

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from "@material-ui/icons";
 import { FC } from "react";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
@@ -23,17 +24,16 @@ const Top = styled.div`
 `;
 
 interface TopButtonProps {
-  fill?: string;
+  filled?: boolean;
 }
 
-const TopButton = styled.button`
+const TopButton = styled.button<TopButtonProps>`
   padding: 10px;
   font-weight: 600;
   cursor: pointer;
-  border: ${(props: TopButtonProps) => props.fill === "filled" && "none"};
-  background-color: ${(props: TopButtonProps) =>
-    props.fill === "filled" ? "black" : "transparent"};
-  color: ${(props: TopButtonProps) => props.fill === "filled" && "white"};
+  border: ${(props) => props.filled && "none"};
+  background-color: ${(props) => (props.filled ? "black" : "transparent")};
+  color: ${(props) => props.filled && "white"};
 `;
 
 const TopTexts = styled.div``;
@@ -41,7 +41,7 @@ const TopTexts = styled.div``;
 const TopText = styled.span`
   text-decoration: underline;
   cursor: pointer;
-  margin: 0 10px;
+  margin: 0px 10px;
 `;
 
 const Bottom = styled.div`
@@ -51,6 +51,65 @@ const Bottom = styled.div`
 
 const Info = styled.div`
   flex: 3;
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductDetails = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Description = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductId = styled.span``;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.span``;
+
+const PriceDetails = styled.span`
+  flex: 1;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 20px;
+  margin: 5px;
+`;
+
+const ProductPrice = styled.div`
+  font-weight: 600;
+`;
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
 `;
 
 const Summary = styled.div`
@@ -70,11 +129,63 @@ const Cart: FC = () => {
             <TopText>Shopping Cart(2)</TopText>
             <TopText>Your Whislist</TopText>
           </TopTexts>
-          <TopButton fill="filled">Checkout</TopButton>
+          <TopButton filled={true}>Checkout</TopButton>
         </Top>
         <Bottom>
-          <Info></Info>
-          <Summary></Summary>
+          <Info>
+            <Product>
+              <ProductDetails>
+                <Image src="https://github.com/kowo0403hk/e-commerce-ts/blob/main/client/docs/shoe.png?raw=true" />
+                <Description>
+                  <ProductName>
+                    <strong>Product:</strong> RUNNER SHOES
+                  </ProductName>
+                  <ProductId>
+                    <strong>ID:</strong> 1234567
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <strong>Size:</strong> 39
+                  </ProductSize>
+                </Description>
+              </ProductDetails>
+              <PriceDetails>
+                <ProductAmountContainer>
+                  <Remove />
+                  <ProductAmount>2</ProductAmount>
+                  <Add />
+                </ProductAmountContainer>
+                <ProductPrice>$79.99</ProductPrice>
+              </PriceDetails>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetails>
+                <Image src="https://i.pinimg.com/originals/2d/af/f8/2daff8e0823e51dd752704a47d5b795c.png" />
+                <Description>
+                  <ProductName>
+                    <b>Product:</b> HAKURA T-SHIRT
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 93813718293
+                  </ProductId>
+                  <ProductColor color="gray" />
+                  <ProductSize>
+                    <b>Size:</b> M
+                  </ProductSize>
+                </Description>
+              </ProductDetails>
+              <PriceDetails>
+                <ProductAmountContainer>
+                  <Remove />
+                  <ProductAmount>1</ProductAmount>
+                  <Add />
+                </ProductAmountContainer>
+                <ProductPrice>$ 29.99</ProductPrice>
+              </PriceDetails>
+            </Product>
+          </Info>
+          <Summary>Summary</Summary>
         </Bottom>
       </Wrapper>
       <Footer />
