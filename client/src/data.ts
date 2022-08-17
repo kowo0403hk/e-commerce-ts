3eb90cd78fb224076997c3730797b1a2cb896a28
Alex Cheung <alexc.sfu@gmail.com>
2022-08-17 01:45:33 -0700

    updated photo format

--- client/src/data.ts
+++ client/src/data.ts
@@ -1,21 +1,21 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://livestockvet.com/storage/app/media/_Client/6h.jpg",
+    img: "https://github.com/kowo0403hk/e-commerce-ts/blob/main/client/docs/Slider1.jpeg?raw=true",
     title: "Oink.",
     desc: "Be an owner this summer!",
     bg: "ffffff",
   },
   {
     id: 2,
-    img: "https://media.istockphoto.com/photos/funny-little-pigs-on-the-farm-picture-id1199307932?k=20&m=1199307932&s=612x612&w=0&h=nrop3C49nHhoX-IqXLJ8ja2PnYUZstF8cTotvPH0QyA=",
+    img: "https://github.com/kowo0403hk/e-commerce-ts/blob/main/client/docs/Slider2.jpeg?raw=true",
     title: "Own a double.",
     desc: "It is more fun to have more piggies.",
     bg: "fcf1ed",
   },
   {
     id: 3,
-    img: "https://www.seriouseats.com/thmb/P4bRGUxGvniOt1B0EUcNZ_99VVk=/458x458/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__images__2013__07__20130709-pig-eating-ice-cream-7a14fc51240a4d859379f51768f99b33.jpg",
+    img: "https://github.com/kowo0403hk/e-commerce-ts/blob/main/client/docs/Slider3.jpg?raw=true",
     title: "MINI!",
     desc: "Imagine a pig that never grows.",
     bg: "fbf0f4",
@@ -25,13 +25,13 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: "https://i.pinimg.com/originals/e8/48/86/e8488690d04c2fa3c03e0ff1cdae8b26.jpg",
+    img: "https://github.com/kowo0403hk/e-commerce-ts/blob/main/client/docs/Categories1.tiff?raw=true",
     title: "PINK FEVER",
     cat: "pearl",
   },
   {
     id: 2,
-    img: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F13%2F2015%2F03%2F11%2Fmicro-pig-LondonPignic.jpg&q=60",
+    img: "https://github.com/kowo0403hk/e-commerce-ts/blob/main/client/docs/Categories1.tiff?raw=true",
     title: "SPOTTED LOVE",
     cat: "buchi",
   },
