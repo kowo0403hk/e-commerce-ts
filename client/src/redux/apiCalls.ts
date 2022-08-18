9d575237fcf4c84d01e0442c749be4e029c44ceb
Alex Cheung <alexc.sfu@gmail.com>
2022-08-18 00:32:49 -0700

    fixed login bug

--- client/src/redux/apiCalls.ts
+++ client/src/redux/apiCalls.ts
@@ -6,9 +6,12 @@ export const login = async (
   user: { username: string | undefined; password: string | undefined }
 ) => {
   dispatch(loginStart());
-
+  console.log(user);
   try {
-    const res = await apiRequest.post("/auth/loin", { user });
+    const res = await apiRequest.post("/auth/login", {
+      username: user.username,
+      password: user.password,
+    });
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
